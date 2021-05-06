0f167451cf81f57601d9797ac3bb16a6d0e80153
Cristian Medina <cristian.daniel.medina@correounivalle.edu.co>
2021-05-06 16:32:45 -0500

    modify useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,18 +3,24 @@ import { useState, useEffect } from 'react'
 export const useFetch = (url) => {
     const [result, setResult] = useState({ data: undefined, loading: true })
 
-    useEffect(async () => {
-        try {
-            const resp = await fetch(url)
-            const d = await resp.json()
-            if (d) {
-               setTimeout(setResult({ loading: false, data: d }),1000)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const resp = await fetch(url)
+                const d = await resp.json()
+                if (d) {
+                   setTimeout(setResult({ loading: false, data: d }),1000)
+                }
+            } catch (e) {
+                console.log('Ocurrio un error:', e)
+                setResult({ loading: false, data: undefined })
             }
-        } catch (e) {
-            console.log('Ocurrio un error:', e)
-            setResult({ loading: false, data: undefined })
         }
+        fetchData();
     }, [url])
+
+
+    
 
     return result
 
